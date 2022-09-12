0ce24c910d5b025eb26b289f0b30485443ec0241
Adityaraj Chauhan <chauhanadi.9@gmail.com>
2022-09-11 19:08:16 -0500

    added chatbox component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ChatBox from './components/ChatBox';
+
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Render Chatbox here
+        </p>
+        <ChatBox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChatBox.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+const ChatBox =()=>{ 
+    return(
+        <h1>ChatBox</h1>
+    );
+}
+
+export default ChatBox;
