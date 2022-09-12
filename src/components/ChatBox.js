cdf76a27e9dfd31153a2b16ee93b71e3faf25f4c
Adityaraj Chauhan <chauhanadi.9@gmail.com>
2022-09-11 22:48:23 -0500

    added automated resposne|| working on chat text display logic

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,27 +1,42 @@
 import React, { useState , useEffect} from 'react'
 // import CloseIcon from '@mui/icons-material/Close';
 import {CgClose} from 'react-icons/cg';
-import { TextField } from '@mui/material';
+import { Input } from '@mui/material';
+import {automatedResponse} from './ChatBoxComponents/automatedResponses';
 import {IoIosSend} from 'react-icons/io'
 const ChatBox =()=>{ 
 
-    const [messages, setMessages] = useState([]);
+    const [automatedResponses, setAutomatedResponses] = useState(automatedResponse);
+    const [userInput, setUserInput]= useState('') ;
+    const [isLoading, setisLoading]= useState(false);
+    const [chatText, setChatText] = useState([]);
+
+    const handleSubmit=(userInput)=>{
+        setChatText(chatText.concat(userInput));
+        setUserInput('');
+    }
 
     useEffect(()=>{
-
+        // console.log(automatedResponses);
     },)
-
     return(
         <div className='chat-window'>
          <div className='titlebar'>
                 <p className='titlebar-text' style={{margin:0}}>Chat</p> 
                 <CgClose className='close' size={24} color='white'/>
          </div>
-            {console.log(messages)}
          <div className='text-container'>
-          
+     
          </div>
-         <div className='text-input'><input className='iotext'></input><button><IoIosSend color='purple' size={24}/></button></div> 
+         <div className='text-input'>
+            <Input className='textField' 
+            fullWidth={true} 
+            size={14} 
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            placeholder='How can we help you today?'
+            multiline={true}/>
+            <button onClick={()=>handleSubmit(userInput)} type='submit'><IoIosSend color='purple' size={24}/></button></div> 
     
         </div>
     );
---
+++ src/components/ChatBoxComponents/automatedResponses.js
@@ -0,0 +1,6 @@
+export const automatedResponse = ['Hi! let us know how we can help and we will have someone connected to you right away',
+                                   'Let me know the topic you need more information on so I can help',
+                                   'Great we will have you connected with someone as soon as we can ',
+                                    'Hi! my name is Roberta, can I get your name?'];
+
+export default {automatedResponse};
