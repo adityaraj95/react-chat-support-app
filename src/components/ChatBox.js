c07b5faa5cb74859be3c7b877b73855e2d8f0eaa
Adityaraj Chauhan <chauhanadi.9@gmail.com>
2022-09-12 19:07:16 -0500

    added chat rendering section || TODO : loading spinning and automated response per user input

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -4,29 +4,73 @@ import {CgClose} from 'react-icons/cg';
 import { Input } from '@mui/material';
 import {automatedResponse} from './ChatBoxComponents/automatedResponses';
 import {IoIosSend} from 'react-icons/io'
+import { render } from '@testing-library/react';
+
 const ChatBox =()=>{ 
 
     const [automatedResponses, setAutomatedResponses] = useState(automatedResponse);
     const [userInput, setUserInput]= useState('') ;
     const [isLoading, setisLoading]= useState(false);
+    const [noInputText , setNoInputText] = useState(true);
     const [chatText, setChatText] = useState([]);
 
     const handleSubmit=(userInput)=>{
-        setChatText(chatText.concat(userInput));
-        setUserInput('');
+        if(userInput.length>0){
+            let _userMsg = { data : userInput , createdBy : '$user'}
+            setNoInputText(false);
+            setChatText(chatText.concat(_userMsg));
+            setUserInput('');
+        }
+     
     }
 
+    const renderChat=(chatText)=>{
+        // console.log(chatText);
+        if(chatText.length>0){
+            return chatText.map(data => {
+                if(data.createdBy === '$user'){
+                   
+                   return <div className='user-text'> {data.data} </div>
+    
+                }else{
+                  return <div className='system-text'>{data.data} </div>
+                }
+             }   
+               
+            );
+        }else{
+            return null;
+        }
+   
+    }
     useEffect(()=>{
-        // console.log(automatedResponses);
-    },)
-    return(
+        setTimeout(() => {
+            if(noInputText){
+                setChatText(chatText.concat(automatedResponse));
+                setNoInputText(false);
+            }
+           
+         }, 3000);
+    
+    },[noInputText])
+
+    useEffect(()=>{
+        setChatText(chatText.concat(automatedResponses[0]));
+        // console.log('initial chattext', chatText)
+    },[])
+
+    let chat= chatText && renderChat(chatText);
+    return( 
         <div className='chat-window'>
          <div className='titlebar'>
                 <p className='titlebar-text' style={{margin:0}}>Chat</p> 
                 <CgClose className='close' size={24} color='white'/>
          </div>
          <div className='text-container'>
-     
+            {/* { chatText && chatText.map(data =>{
+                if(data.createdBy){}else{}
+            })} */}
+            {chat}
          </div>
          <div className='text-input'>
             <Input className='textField' 
@@ -36,7 +80,7 @@ const ChatBox =()=>{
             onChange={(e) => setUserInput(e.target.value)}
             placeholder='How can we help you today?'
             multiline={true}/>
-            <button onClick={()=>handleSubmit(userInput)} type='submit'><IoIosSend color='purple' size={24}/></button></div> 
+            <button className='submit-button' onClick={()=>handleSubmit(userInput)} type='submit'><IoIosSend color='purple' size={24}/></button></div> 
     
         </div>
     );
--- src/components/ChatBoxComponents/automatedResponses.js
+++ src/components/ChatBoxComponents/automatedResponses.js
@@ -1,6 +1,4 @@
-export const automatedResponse = ['Hi! let us know how we can help and we will have someone connected to you right away',
-                                   'Let me know the topic you need more information on so I can help',
-                                   'Great we will have you connected with someone as soon as we can ',
-                                    'Hi! my name is Roberta, can I get your name?'];
+export const automatedResponse = [{ data : 'Hi! let us know how we can help and we will have someone connected to you right away', createdBy : '$system'},
+                                  {data : 'Let me know the topic you need more information on so I can help', createdBy : '$system'},];
 
 export default {automatedResponse};
